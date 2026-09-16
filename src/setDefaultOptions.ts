05a6f5586d7fa1c988a25de334fc14e689e07637
Marco Braak <marco@floryn.com>
2026-09-16 09:48:25 +0200

    Do not set undefined values in default options

--- src/setDefaultOptions.ts
+++ src/setDefaultOptions.ts
@@ -11,35 +11,35 @@ const defaults: TreeElementOptions = {
   classPrefix: DEFAULT_CLASS_PREFIX, // the prefix of all css classes
   // The symbol to use for a closed node - ► BLACK RIGHT-POINTING POINTER
   // http://www.fileformat.info/info/unicode/char/25ba/index.htm
-  closedIcon: undefined,
-  commonClassName: undefined, // the class of every element; the default is "<classPrefix>-common"
-  data: undefined,
-  dataFilter: undefined,
-  dataUrl: undefined,
+  // closedIcon: undefined,
+  // commonClassName: undefined, // the class of every element; the default is "<classPrefix>-common"
+  // data: undefined,
+  // dataFilter: undefined,
+  // dataUrl: undefined,
   dragAndDrop: false,
   keyboardSupport: true,
   nodeClass: Node,
-  onCanMove: undefined, // Can this node be moved?
-  onCanMoveTo: undefined, // Can this node be moved to this position? function(moved_node, target_node, position)
-  onCanSelectNode: undefined,
-  onCreateLi: undefined,
-  onDragMove: undefined,
-  onDragStop: undefined,
-  onGetStateFromStorage: undefined,
-  onIsMoveHandle: undefined,
-  onSetStateFromStorage: undefined,
-  openedIcon: undefined,
+  // onCanMove: undefined, // Can this node be moved?
+  // onCanMoveTo: undefined, // Can this node be moved to this position? function(moved_node, target_node, position)
+  // onCanSelectNode: undefined,
+  // onCreateLi: undefined,
+  // onDragMove: undefined,
+  // onDragStop: undefined,
+  // onGetStateFromStorage: undefined,
+  // onIsMoveHandle: undefined,
+  // onSetStateFromStorage: undefined,
+  // openedIcon: undefined,
   openFolderDelay: 500, // The delay for opening a folder during drag and drop; the value is in milliseconds
   // The symbol to use for an open node - ▼ BLACK DOWN-POINTING TRIANGLE
   // http://www.fileformat.info/info/unicode/char/25bc/index.htm
-  rtl: undefined, // right-to-left support; true / false (default)
+  // rtl: undefined, // right-to-left support; true / false (default)
   saveState: false, // true / false / string (local storage key; the default key is "tree")
   selectable: true,
   showEmptyFolder: false,
   slide: true, // must display slide animation?
   startDndDelay: 300, // The delay for starting dnd (in milliseconds)
   tabIndex: 0,
-  treeClassName: undefined, // the class of the root element; the default is classPrefix
+  // treeClassName: undefined, // the class of the root element; the default is classPrefix
   useContextMenu: true,
 };
 
