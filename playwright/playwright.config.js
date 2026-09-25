81b0b6c6fc91214f7465ddb6a5aa68c1705abcae
Marco Braak <marco@floryn.com>
2026-09-25 08:53:07 +0200

    Remove unused comment

--- playwright/playwright.config.js
+++ playwright/playwright.config.js
@@ -1,19 +1,18 @@
-//const { devices } = require("@playwright/test");
 import { devices } from "@playwright/test";
 
 const config = {
-    projects: [
-        {
-            name: "Chromium",
-            use: { ...devices["Desktop Chrome"] },
-        },
-    ],
-    testDir: "./",
-    webServer: {
-        command: "pnpm devserver-with-coverage",
-        cwd: "..",
-        port: 8080,
+  projects: [
+    {
+      name: "Chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
+  ],
+  testDir: "./",
+  webServer: {
+    command: "pnpm devserver-with-coverage",
+    cwd: "..",
+    port: 8080,
+  },
 };
 
 export default config;
